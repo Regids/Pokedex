c78e1c817edd0c4d05423ce1fdb333c1746f4106
Daniel <josuerd@hotmail.com>
2023-02-18 19:38:27 -0600

    Api and list

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Pokedex.GetApis;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            ListPokemon listpoke = new ListPokemon();
+            ClassPokedex poke = new ClassPokedex();
+            List<ClassPokedex> ListPoke = new List<ClassPokedex>();
+
+            string[] pokes = listpoke.convertToList();
+
+            foreach(string pokemon in pokes)
+            {
+                string url = "https://pokeapi.co/api/v2/pokemon/" + pokemon.ToLower();
+                HttpClient client = new HttpClient();
+
+                var httpResponse = await client.GetAsync(url);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    var content = await httpResponse.Content.ReadAsStringAsync();
+
+                    poke = JsonConvert.DeserializeObject<ClassPokedex>(content);
+                    ListPoke.Add(poke);
+                }
+                else
+                {
+                    Console.WriteLine("El pokemon: {0} no existe.", pokemon);
+                }
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
